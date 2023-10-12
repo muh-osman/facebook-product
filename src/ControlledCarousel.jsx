27966fc9c575b8e4489d7850ce48fe6b45a353c2
Muhammed Osman <91621437+Muh-Osman@users.noreply.github.com>
2023-10-13 00:27:43 +0300

    Fix

--- src/ControlledCarousel.jsx
+++ src/ControlledCarousel.jsx
@@ -21,7 +21,7 @@ function ControlledCarousel() {
     <Carousel className={style.carousel} activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
         <div className="img-container">
-          <img className={style.carousel_img} src={Carouse1} alt="Carousel" text="First slide" />
+          <img className={style.carousel_img} src={Carouse2} alt="Carousel" text="First slide" />
         </div>
         <Carousel.Caption>
           <h1>عرض حصري وفر 50% عند طلبك خدماتنا لأول مرة.</h1>
@@ -29,10 +29,10 @@ function ControlledCarousel() {
       </Carousel.Item>
       <Carousel.Item>
         <div className="img-container">
-          <img className={style.carousel_img} src={Carouse2} alt="Carousel" text="Second slide" />
+          <img className={style.carousel_img} src={Carouse1} alt="Carousel" text="Second slide" />
         </div>
         <Carousel.Caption>
-        <h1>نستخدم الأساليب المستدامة. طاقة ومياه وبلاستيك أقل.</h1>
+        <h1>نستخدم أساليب مستدامة. طاقة ومياه وبلاستيك أقل.</h1>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -40,7 +40,7 @@ function ControlledCarousel() {
           <img className={style.carousel_img} src={Carouse3} alt="Carousel" text="Third slide" />
         </div>
         <Carousel.Caption>
-        <h1>لا تستخدم مواد كيميائية ضارة.</h1>
+        <h1>لا نستخدم مواد كيميائية ضارة.</h1>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
